5803092a04ca140e1c4157e6ce0a6dfbbd6443f7
VirenP26 <129236919+VirenP26@users.noreply.github.com>
2023-03-28 20:56:19 -0400

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,11 @@
+public class TicTacToe {
+
+    public static void Main(String args[]){
+
+        char[][] board = {{' ',' ',' '},
+                          {' ',' ',' '},
+                          {' ',' ',' '}};
+
+        System.out.println(board[0][0]);
+    }
+}
